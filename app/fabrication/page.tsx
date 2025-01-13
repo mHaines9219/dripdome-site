c903c4065a0f8e1c3f85aca06cecc4b0a90a922f
mHaines331 <mhaines9219@gmail.com>
2025-01-12 21:02:41 -0500

    fabrication styling

--- app/fabrication/page.tsx
+++ app/fabrication/page.tsx
@@ -2,9 +2,23 @@
 import React from 'react';
 import { Typography } from '@mui/material';
 import FabricationCarousel from '../components/FabricationCarousel';
+import { motion } from 'framer-motion';
+
 import Contact from '../components/Contact';
 
 export default function page() {
+  const services = [
+    'Carpentry',
+    'Metalwork',
+    'Scenic Painting',
+    'Murals',
+    'CNC Routing',
+    '3D Renders',
+    'Props',
+    'Graphic Design',
+    'Printing',
+    'LED/Light Integration',
+  ];
   return (
     <>
       <Typography
@@ -36,11 +50,35 @@ export default function page() {
           textAlign: 'center',
         }}
       >
-        We specialize in creating unforgettable set designs and custom
-        fabrications for photoshoots that demand visual excellence. From concept
-        to construction, our team works with photographers, stylists, and brands
-        to craft striking, camera-ready environments that captivate audiences.
+        We specialize in turning creative ideas into stunning realities. Our
+        skilled team combines artistry and precision to fabricate custom
+        solutions tailored to your unique needs. Whether you're creating
+        immersive sets, intricate props, or large-scale installations, we have
+        the expertise to make it happen.
       </Typography>
+      <div className="w-full max-w-4xl mx-auto p-4">
+        <div className="grid grid-cols-2 gap-4 text-lg text-gray-700 my-8">
+          {services.map((service, index) => (
+            <motion.div
+              key={index}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: index * 0.1, duration: 0.5 }}
+            >
+              <Typography
+                variant="h2"
+                align="center"
+                sx={{
+                  fontSize: { xs: '20px', md: '25px', lg: '35px' },
+                }}
+              >
+                {service.toUpperCase()}
+              </Typography>
+            </motion.div>
+          ))}
+        </div>
+      </div>
+
       <FabricationCarousel />
       <div className="bg-black  py-10">
         <Contact />
