6c4aeee5983b5518ab7b2d79ce2d7e668c95c8b3
mHaines331 <mhaines9219@gmail.com>
2025-02-06 16:54:51 -0800

    fix navbar on other pages

--- app/ui/Navbar.tsx
+++ app/ui/Navbar.tsx
@@ -15,26 +15,28 @@ export default function Navbar() {
     { name: 'ABOUT US', href: '/about-us' },
     { name: 'PORTFOLIO', href: '/portfolio' },
     { name: 'SERVICES', href: '/services' },
-    // { name: 'CONVENTIONS', href: '/convenions' },
     { name: 'RENTALS', href: '/rentals' },
   ];
 
   const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
-    <nav className="bg-black  font-nova text-white fixed top-0 left-0 w-screen z-50 h-[95px] flex items-center md:pr-[43px]">
-      <div className="w-full flex items-center justify-between md:justify-start mt-[19px] mb-[19px] pl-4">
-        <Link href="/" className="flex items-center space-x-3">
+    <nav className="bg-black font-nova text-white fixed top-0 left-0 w-screen z-50 h-[95px] md:h-[120px] flex items-center md:pr-[43px]">
+      <div className="w-full flex items-center justify-between md:justify-start mt-[19px] mb-[19px] pl-4 md:mt-[30px] md:mb-[30px]">
+        <Link href="/" className="flex items-center space-x-3 md:hidden">
           <HomeOutlinedIcon className="h-[45px] w-[45px] rounded-xl bg-black bg-opacity-60 p-2" />
         </Link>
-        <Image
-          alt="Company Logo"
-          src="/assets/dd_logo.png"
-          width={90}
-          height={90}
-        ></Image>
+        <div className="flex-grow flex justify-center md:justify-start">
+          <Image
+            alt="Company Logo"
+            src="/assets/dd_logo.png"
+            width={90}
+            height={90}
+            className="md:ml-4 md:w-[120px] md:h-[120px]"
+          />
+        </div>
         <button
-          className="md:hidden text-white mr-6 text-3xl  z-50"
+          className="md:hidden text-white mr-6 text-3xl z-50"
           onClick={toggleMenu}
         >
           ☰
@@ -48,7 +50,7 @@ export default function Navbar() {
             key={link.name}
             href={link.href}
             className={clsx(
-              'flex h-[48px] items-center justify-center gap-2 p-3 text-sm font-medium transition hover:text-[var(--main-gold)] whitespace-nowrap',
+              'flex h-[48px] items-center justify-center gap-2 p-3 text-lg font-medium transition hover:text-[var(--main-gold)] whitespace-nowrap',
               {
                 'border-b-2 border-yellow-500': pathname === link.href,
                 'text-white': pathname !== link.href,
@@ -61,10 +63,9 @@ export default function Navbar() {
       </ul>
 
       {/* Mobile Menu */}
-      {/* Mobile Menu */}
       <ul
         className={clsx(
-          'md:hidden fixed top-0 -mt-12 left-0 w-screen h-screen flex flex-col items-center bg-black  justify-center bg-[var(--main-brown)] transition-all ease-in-out duration-300',
+          'md:hidden fixed top-0 -mt-12 left-0 w-screen h-screen flex flex-col items-center bg-black justify-center bg-[var(--main-brown)] transition-all ease-in-out duration-300',
           {
             'opacity-100 pointer-events-auto': isOpen, // Show menu when open
             'opacity-0 pointer-events-none': !isOpen, // Hide menu when closed
