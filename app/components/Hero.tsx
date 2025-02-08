db9b3fc76793ce7514c1da90f72ad3a0daf6d2d8
mHaines331 <mhaines9219@gmail.com>
2025-02-08 10:36:35 -0800

    icon fix

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -27,7 +27,10 @@ export default function Hero() {
         <nav className="absolute top-0 left-0 w-full bg-[var(--main-brown)] font-nova text-white z-50 h-[95px] flex items-center">
           <div className="w-full flex items-center justify-between mt-[19px] mb-[19px] pl-4">
             <Link href="/" className="flex items-center space-x-3">
-              <HomeTwoToneIcon className="h-[45px] w-[45px] rounded-xl bg-black bg-opacity-60 p-2" />
+              <HomeTwoToneIcon
+                fontSize="inherit"
+                className="h-[45px] w-[45px] rounded-xl bg-black bg-opacity-60 p-2"
+              />
             </Link>
             <button
               className="text-white text-3xl bg-opacity-60 pb-2 px-2 mr-8 z-50 bg-black rounded-xl"
