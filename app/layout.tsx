020817ddd85d45c153d3681566d72d433c34387f
mHaines <mhaines9219@gmail.com>
2025-05-13 17:04:06 -0700

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,37 @@
-import type { Metadata } from 'next';
-import localFont from 'next/font/local';
-import './globals.css';
-import ThemeProviderWrapper from './ThemeProviderWrapper';
-import Navbar from './ui/Navbar';
-import ClientLayout from './ClientLayout';
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import ThemeProviderWrapper from "./ThemeProviderWrapper";
+import Navbar from "./ui/Navbar";
+import ClientLayout from "./ClientLayout";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title:
-    'DripDome | Set Design & Production Design Collective in Los Angeles and NYC',
+    "DripDome | Set Design & Production Design Collective in Los Angeles and NYC",
   description:
-    'Premier set design and production design studio in Los Angeles. Professional fabrication and creative services for media professionals. Custom solutions for film, television, and commercial projects.',
+    "Premier set design and production design studio in Los Angeles. Professional fabrication and creative services for media professionals. Custom solutions for film, television, and commercial projects.",
   keywords:
-    'set design, production design, Los Angeles, fabrication, creative services, media production, studio services, custom sets, film sets, TV production',
+    "set design, production design, Los Angeles, fabrication, creative services, media production, studio services, custom sets, film sets, TV production",
   openGraph: {
-    title: 'DripDome | Set Design & Production Design Studio Los Angeles',
+    title: "DripDome | Set Design & Production Design Studio Los Angeles",
     description:
-      'Professional set design, production design, and fabrication services in Los Angeles.',
-    type: 'website',
-    locale: 'en_US',
-    siteName: 'DripDome',
+      "Professional set design, production design, and fabrication services in Los Angeles.",
+    type: "website",
+    locale: "en_US",
+    siteName: "DripDome",
     images: [
       {
-        url: 'https://dripdome-site.s3.us-east-2.amazonaws.com/dripdome_logo.png',
+        url: "https://dripdome-site.s3.us-east-2.amazonaws.com/dripdome_logo.png",
         width: 1200,
         height: 630,
-        alt: 'DripDome Set Design & Production in Los Angeles',
+        alt: "DripDome Set Design & Production in Los Angeles",
       },
     ],
   },
-  robots: 'index, follow',
-  viewport: 'width=device-width, initial-scale=1',
-  themeColor: '#ffffff',
+  robots: "index, follow",
+  viewport: "width=device-width, initial-scale=1",
+  themeColor: "#ffffff",
 };
 
 export default function RootLayout({
