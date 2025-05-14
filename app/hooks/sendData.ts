c000d2dacd0bfedd5fd2f880384ca92b11950437
mHaines <mhaines9219@gmail.com>
2025-05-13 17:53:57 -0700

    update prod url for n7n

--- app/hooks/sendData.ts
+++ app/hooks/sendData.ts
@@ -6,8 +6,9 @@ type formData = {
 };
 
 export const sendData = async (data: formData) => {
+  console.log(data);
   await fetch(
-    "https://kingbird.app.n8n.cloud/webhook-test/653ffa9b-9b49-4ada-870d-f74a27ad2835",
+    "https://kingbird.app.n8n.cloud/webhook/653ffa9b-9b49-4ada-870d-f74a27ad2835",
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
